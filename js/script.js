cbaa1247f2800be70aaeaff70c9de87c948a3c78
MikeHapkdoIn <mangelveragomez@gmail.com>
2025-03-11 13:37:41 +0100

    Challenge S3U2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
- // Aquí tu código
+// Aquí tu código
+const addButton = document.getElementById("agregar");
+const lista = document.getElementById("lista");
+
+addButton.addEventListener("click", function() {
+    const element = prompt ("Agregar elemento:");
+    
+    
+    if (element !== null && element !== "") {
+ 
+        const nuevoElemento = document.createElement("li");
+ 
+        nuevoElemento.textContent = element;
+    
+        lista.appendChild( nuevoElemento);}
+ 
+        else {
+ 
+     
+         alert ("No agregaste nada a la lista");
+     } 
+    });
+ 
